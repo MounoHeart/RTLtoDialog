914e287b4c236d38ee9cd9e3719a80823618873e
MounoHeart <85218086+MounoHeart@users.noreply.github.com>
2021-07-02 09:53:17 +0300

    Update RTLtoDialog.py

--- RTLtoDialog.py
+++ RTLtoDialog.py
@@ -9,3 +9,5 @@ class RTLtoDialogCommand(sublime_plugin.EventListener):
         	ch = view.substr(region1)
         	if ('\u0600' <= ch <= '\u06FF' or '\u0750' <= ch <= '\u077F' or '\u08A0' <= ch <= '\u08FF' or '\uFB50' <= ch <= '\uFDFF' or '\uFE70' <= ch <= '\uFEFF' or '\U00010E60' <= ch <= '\U00010E7F' or '\U0001EE00' <= ch <= '\U0001EEFF'):
         		sublime.message_dialog(ch)
+                self.view.show_popup(ch) #Shows a popup displaying HTML content.
+
